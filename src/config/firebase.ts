2622e48b33432bd0c8b94c484957d6c51cff71d0
code3743 <jota.lopez@correounivalle.edu.co>
2024-09-14 16:21:14 -0500

    feat: update Firebase configuration and dependencies, add .env and service account to .gitignore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,11 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
+import { applicationDefault, initializeApp } from "firebase-admin/app";
 
 
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(
+  {
+    credential: applicationDefault(),
+  }
+);
 
 export default app;
