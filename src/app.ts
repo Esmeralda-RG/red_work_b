fd1a98897eb406110e9886c76902030e95e40737
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-08 15:17:26 -0500

    Add socket.io server integration and handle client connections

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
 import express  from "express";
 import router from "./routers/router";
+import {Server} from "socket.io";
+import http from "http";
 import cors from "cors";
+import SocketsClients from "./sockes";
+
 
 const app = express();
+const server = http.createServer(app);
+const socketsClients = new SocketsClients();
+const io = new Server(server);     
+
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', router);
+io.on('connection', (socket) => {
+
+    const id = socket.handshake.query.id?.toString();
+    if(id){
+       socketsClients.setSocket(socket, id);
+    }
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
